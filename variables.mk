399f5ac5290df9b77a2e41cec988066d19cce13b
ichmy55 <ichmy@nifty.com>
2026-09-22 07:47:25 +0900

    #44 2026-09-22-01

--- variables.mk
+++ variables.mk
@@ -4,4 +4,4 @@
 DOCKER_IMAGE  := ghcr.io/ichmy55/ichmy55.github.io/httpd:main
 DOCKER_IMAGE_LOCAL  := ichmy55-httpd:latest
 DOCKER_NAME   := httpd
-PACKAGE_USE   := 1              # 出来合いパッケージを使用するには"1"をセット
+PACKAGE_USE   := 0              # 出来合いパッケージを使用するには"1"をセット
